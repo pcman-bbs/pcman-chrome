8bf46fcac9abacf7b1d7df6ed39d97cbe07549d7
u881831 <u881831@hotmail.com>
2016-01-01 00:47:50 +0800

    Enable SSH automatically when the site uses SSH
    Merged from 66dfd17 of branch pref_test of pcman-firefox
    Use username 'bbs' automatically for
    ptt.cc:22
    ptt2.cc:22
    ptt3.cc:22
    Prompt for UserPass similarly to putty for other sites

--- public_html/conn.js
+++ public_html/conn.js
@@ -64,9 +64,21 @@ Conn.prototype={
         }
         this.isConnected = false;
 
-        // FIXME: connect to other site or port
-        var _this = (host == 'ptt.cc' && port == 22) ? this : null;
-        this.ssh = new SSH(_this, 'bbs', 'bbs', function(status, message){
+        var login = '';
+        var password = '';
+        if(port == 22) {
+            switch(host) {
+            case 'ptt.cc':
+            case 'ptt2.cc':
+            case 'ptt3.cc':
+                login = 'bbs';
+                password = 'bbs';
+                break;
+            default:
+            }
+        }
+        var _this = this;
+        this.ssh = new SSH(_this, login, password, function(status, message){
             switch(status) {
             case 'onConnected':
             case 'loginAccepted':
